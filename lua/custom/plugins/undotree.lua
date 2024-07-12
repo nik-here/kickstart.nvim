1890500a01c890b7926707b7ba33626049ceb110
Nik Kiene <nik.kiene.76@gmail.com>
2024-07-12 17:47:38 +0200

    swap = false

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -1,6 +1,7 @@
 return {
   'mbbill/undotree',
   init = function()
+    vim.opt.swapfile = false
     vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
     vim.opt.undofile = true
     vim.keymap.set('n', '<leader>u', function()
